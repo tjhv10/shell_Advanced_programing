296d50debf534557243a8e0527bf1ab14669b219
nitayGitHub <nitaylevi1998@gmail.com>
2024-05-23 16:06:49 +0300

    Added Commands History

--- shell.c
+++ shell.c
@@ -1,12 +1,13 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-#include "stdio.h"
-#include "errno.h"
-#include "stdlib.h"
-#include "unistd.h"
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <termios.h>
 
 #define OUT 0
 #define APP 1
@@ -39,8 +40,13 @@
 #define READ_STR "read"
 #define PROMPT_STR_AFTER ": "
 
+#define HISTORY_SIZE 100
+
 int status;
 char *prompt = HELLO;
+char *command_history[HISTORY_SIZE];
+int history_count = 0;
+int history_index = -1;
 
 void c_handler(int);
 void pipe_tasks(char *);
@@ -48,30 +54,59 @@ void async_tasks(char *);
 void redirect_tasks(char *, int);
 void other_tasks(char *);
 void set_variable(char *, char *);
-char * get_variable(char *);
+char *get_variable(char *);
 void parse_if_statement(char *);
+int parse_command(char **parsed_command, char *cmd, const char *delimiter);
 
-int parse_command(char **parsed_command, char *cmd, const char *delimeter) {
-    char *cmd_copy = strdup(cmd); // Create a copy of the original command string
+void add_to_history(char *command) {
+    if (history_count < HISTORY_SIZE) {
+        command_history[history_count++] = strdup(command);
+    } else {
+        free(command_history[0]);
+        for (int i = 1; i < HISTORY_SIZE; ++i) {
+            command_history[i - 1] = command_history[i];
+        }
+        command_history[HISTORY_SIZE - 1] = strdup(command);
+    }
+}
+
+char *get_previous_command() {
+    if (history_index > 0) {
+        return command_history[--history_index];
+    } else if (history_index == 0) {
+        return command_history[history_index];
+    }
+    return "";
+}
+
+char *get_next_command() {
+    if (history_index < history_count - 1) {
+        return command_history[++history_index];
+    }
+    return "";
+}
+
+int parse_command(char **parsed_command, char *cmd, const char *delimiter) {
+    char *cmd_copy = strdup(cmd);
     if (!cmd_copy) {
         perror("strdup");
-        return -1; // Return an error if strdup fails
+        return -1;
     }
 
     char *token;
-    token = strtok(cmd_copy, delimeter);
+    token = strtok(cmd_copy, delimiter);
     int counter = -1;
 
-    while(token) {
+    while (token) {
         parsed_command[++counter] = malloc(strlen(token) + 1);
         if (!parsed_command[counter]) {
             perror("malloc");
             free(cmd_copy);
-            return -1; // Return an error if malloc fails
+            return -1;
         }
         strcpy(parsed_command[counter], token);
 
-        if (strcmp(delimeter, PIPE_STR) == 0) {
+        if (strcmp(delimiter, PIPE_STR) == 0) {
             if (parsed_command[counter][strlen(token) - 1] == EMPTY_CHAR) {
                 parsed_command[counter][strlen(token) - 1] = END_L_CHR;
             }
@@ -80,11 +115,11 @@ int parse_command(char **parsed_command, char *cmd, const char *delimeter) {
             }
         }
         parsed_command[counter][strlen(token) + 1] = END_L_CHR;
-        token = strtok(NULL, delimeter);
+        token = strtok(NULL, delimiter);
     }
     parsed_command[++counter] = NULL;
 
-    free(cmd_copy); // Free the copy of the original command string
+    free(cmd_copy);
     return counter;
 }
 
@@ -120,23 +155,23 @@ void pipe_tasks(char *cmd) {
             exit(EXIT_FAILURE);
         }
 
-        if (pid == 0) { // Child process
-            if (i != 0) { // Not the first command
+        if (pid == 0) {
+            if (i != 0) {
                 dup2(fd[0], 0);
                 close(fd[0]);
             }
-            if (i != commands - 1) { // Not the last command
+            if (i != commands - 1) {
                 dup2(fd[1], 1);
                 close(fd[1]);
             }
             execvp(inner_cmd[0], inner_cmd);
             perror("execvp");
             exit(EXIT_FAILURE);
-        } else { // Parent process
-            wait(NULL); // Wait for the child process to finish
+        } else {
+            wait(NULL);
             if (i != commands - 1) {
                 close(fd[1]);
-                fd[0] = fd[0]; // Preserve the read end of the pipe for the next iteration
+                fd[0] = fd[0];
             }
         }
     }
@@ -152,34 +187,30 @@ void async_tasks(char *cmd) {
 }
 
 void redirect_tasks(char *command, int direction) {
-    if (fork() == 0) { // child
+    if (fork() == 0) {
         char *parsed_command[LINE_COMMAND_SIZE];
         int commands = parse_command(parsed_command, command, EMPTY_STRING);
         int fd;
         switch (direction) {
-            case OUT: // output
+            case OUT:
                 fd = creat(parsed_command[commands - 1], 0660);
                 dup2(fd, 1);
                 break;
-
-            case APP: // append
+            case APP:
                 fd = open(parsed_command[commands - 1], O_CREAT | O_APPEND | O_RDWR, 0660);
                 dup2(fd, 1);
                 break;
-
-            case IN: // input
+            case IN:
                 fd = open(parsed_command[commands - 1], O_RDONLY, 0660);
                 dup2(fd, 0);
                 break;
-
             default:
                 break;
         }
-
         parsed_command[commands - 2] = parsed_command[commands - 1] = NULL;
         execvp(parsed_command[0], parsed_command);
     } else {
-        wait(&status); // wait for child to finish.
+        wait(&status);
     }
 }
 
@@ -223,7 +254,6 @@ void parse_if_statement(char *if_command) {
     char else_block[COMMAND_SIZE] = "";
     int in_then = 0, in_else = 0;
 
-    // Extract condition from the command
     for (int i = 1; parsed_command[i] != NULL; i++) {
         strcat(condition, parsed_command[i]);
         strcat(condition, " ");
@@ -253,7 +283,7 @@ void parse_if_statement(char *if_command) {
         }
     }
 
-    if (system(condition) == 0) { // condition is true
+    if (system(condition) == 0) {
         if (strchr(then_block, PIPE_CHR)) {
             pipe_tasks(then_block);
         } else {
@@ -268,10 +298,9 @@ void parse_if_statement(char *if_command) {
     }
 }
 
-
 void set_variable(char *name, char *value) {
     int sv = setenv(name, value, 1);
-    if (sv) { // error set variable
+    if (sv) {
         perror("Error set env");
     }
 }
@@ -280,23 +309,71 @@ char *get_variable(char *name) {
     return getenv(name);
 }
 
+void clear_line() {
+    printf("\033[2K\033[1G"); // Clear line and move cursor to the beginning
+    printf("%s: ", prompt);
+    fflush(stdout);
+}
+
 int main() {
-    // handler for Ctrl+C
+    struct termios orig_termios, new_termios;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    new_termios = orig_termios;
+    new_termios.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+
     signal(SIGINT, c_handler);
     char command[COMMAND_SIZE], saved_cmd[COMMAND_SIZE];
 
     while (TRUE) {
         printf("%s: ", prompt);
-        fgets(command, COMMAND_SIZE, stdin);
-        command[strlen(command) - 1] = END_L_CHR;
+        fflush(stdout);
 
-        if (!strcmp(command, QUIT)) { // quit
+        int index = 0;
+        char ch;
+        while (read(STDIN_FILENO, &ch, 1) == 1) {
+            if (ch == '\n') {
+                command[index] = END_L_CHR;
+                printf("\n");
+                break;
+            } else if (ch == 27) {
+                char seq[3];
+                if (read(STDIN_FILENO, &seq[0], 1) == 1 && read(STDIN_FILENO, &seq[1], 1) == 1) {
+                    if (seq[0] == '[') {
+                        if (seq[1] == 'A') {
+                            strcpy(command, get_previous_command());
+                            index = strlen(command);
+                            clear_line();
+                            write(STDOUT_FILENO, &command, index);
+                        } else if (seq[1] == 'B') {
+                            strcpy(command, get_next_command());
+                            index = strlen(command);
+                            clear_line();
+                            write(STDOUT_FILENO, &command, index);
+                        }
+                    }
+                }
+            } else {
+                command[index++] = ch;
+                write(STDOUT_FILENO, &ch, 1);
+            }
+        }
+
+        if (index == 0) {
+            continue;
+        }
+
+        add_to_history(command);
+        history_index = history_count;
+
+        if (!strcmp(command, QUIT)) {
             break;
         } else if (!strcmp(command, AGAIN)) {
             strcpy(command, saved_cmd);
         } else {
             strcpy(saved_cmd, command);
         }
+
         if (strchr(command, PIPE_CHR) && !strstr(command, "if")) {
             pipe_tasks(command);
         } else if (strchr(command, AND)) {
@@ -311,4 +388,11 @@ int main() {
             other_tasks(command);
         }
     }
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &orig_termios);
+    for (int i = 0; i < history_count; ++i) {
+        free(command_history[i]);
+    }
+
+    return 0;
 }
